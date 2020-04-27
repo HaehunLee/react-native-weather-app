81e6b3ef24cd854e0e3fffa7ab2ab82725984530
HaehunLee <gnsl1102@gmail.com>
2020-04-28 02:17:13 +0900

    Feat: Fetch Weather API

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,20 @@
-import React, { Fragment } from "react";
-import { StatusBar, SafeAreaView } from 'react-native';
-
-import { Header, LearnMoreLinks, Colors, DebugInstructions, ReloadInstructions } from 'react-native/Libraries/NewAppScreen';
+import React from "react";
 
 import Styled from 'styled-components/native';
+import WeatherView from "./Screens/WeatherView";
 
-const ScrollView = Styled.ScrollView`
+const Container = Styled.View`
+  flex : 1;
+  background-color : #EEE;
 `;
-
-const Body = Styled.View`
-`;
-
-const SectionContainer = Styled.View`
-  margin-top : 32px;
-  padding-horizontal : 24px;
-`;
-
-const SectionDescription = Styled.Text`
-  margin-top : 8px;
-  font-size : 18px;
-  font-weight : 400;
-`;
-
 
 interface Props {}
 
 const App = ({}:Props) => {
   return (
-    <Fragment>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView contentInsetAdjustmentBehavior="automatic">
-          <Header />
-          <Body>
-            <SectionContainer>
-              <SectionDescription>Step One</SectionDescription>
-              <SectionDescription>
-                Edit
-              </SectionDescription>
-            </SectionContainer>
-          </Body>
-        </ScrollView>
-      </SafeAreaView>
-    </Fragment>
+    <Container>
+      <WeatherView />
+    </Container>
   )
 }
 
